f42359c4a50e38ea2f00cd51470ad20ea6f3600b
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-04-20 02:59:06 +0200

    API dox: document Q_DECLARE_FLAGS-based flags
    Summary:
    KApiDox & ECMAddQch have been just teached about Q_DECLARE_FLAGS, so the
    underlying typedefs are no longer skipped by doxygen, but can be
    documented now, allowing links to be generated for these types e.g. when
    used as arguments.
    
    The "#" prefix to the enum name in the description text of all the
    Q_DECLARE_FLAGS docs seems needed to properly trigger doxygen autolinks.
    
    GIT_SILENT

--- src/kcrash.h
+++ src/kcrash.h
@@ -116,6 +116,7 @@ KCRASH_EXPORT HandlerType emergencySaveFunction();
 
 /**
  * Options to determine how the default crash handler should behave.
+ * @see CrashFlags
  */
 enum CrashFlag {
     KeepFDs = 1,          ///< don't close all file descriptors immediately
@@ -123,6 +124,9 @@ enum CrashFlag {
     AlwaysDirectly = 4,   ///< never try to to start DrKonqi via kdeinit. Use fork() and exec() instead. @deprecated This is now the default, and does not need to be set.
     AutoRestart = 8       ///< autorestart this application. Only sensible for KUniqueApplications. @since 4.1.
 };
+/**
+ * Stores a combination of #CrashFlag values.
+ */
 Q_DECLARE_FLAGS(CrashFlags, CrashFlag)
 
 /**
