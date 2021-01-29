65dc10844d76be087780e2aa54b616e1ba51972e
Friedrich W. H. Kossebau <kossebau@kde.org>
2021-01-29 06:34:33 +0100

    Use non-deprecated KDEInstallDirs variable also in code generation
    NO_CHANGELOG

--- src/kcrash.cpp
+++ src/kcrash.cpp
@@ -318,7 +318,7 @@ void KCrash::setDrKonqiEnabled(bool enabled)
             << QFile::decodeName(qgetenv("LIBEXEC_PATH")).split(QLatin1Char(':'), Qt::SkipEmptyParts) // env var is used first
             << QCoreApplication::applicationDirPath() // then look where our application binary is located
             << QLibraryInfo::location(QLibraryInfo::LibraryExecutablesPath) // look where libexec path is (can be set in qt.conf)
-            << QFile::decodeName(CMAKE_INSTALL_FULL_LIBEXECDIR); // look at our installation location
+            << QFile::decodeName(KDE_INSTALL_FULL_LIBEXECDIR); // look at our installation location
         const QString exec = QStandardPaths::findExecutable(QStringLiteral("drkonqi"), paths);
         if (exec.isEmpty()) {
             qCDebug(LOG_KCRASH) << "Could not find drkonqi in search paths:" << paths;
