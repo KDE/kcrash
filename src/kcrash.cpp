21fadd6cf14dae8719ad2bae531c3883da0104a4
Harald Sitter <sitter@kde.org>
2021-06-02 15:24:15 +0200

    let some of the env vars disable all automatic init
    when crash handling as a whole should be disabled by default we really
    should not override that based on the metadata condition. rather, when
    shouldHandleCrash() the crash handler never ought to get set up and
    since metadata writing requires the handler by extension that means the
    metadata ought not get written either
    
    this is ultimately the similar to s_launchDrKonqi: the app opts out.
    while shouldHandleCrash means: the env opts out.
    
    in either case we shouldn't take any steps towards automatic
    initialization

--- src/kcrash.cpp
+++ src/kcrash.cpp
@@ -233,8 +233,12 @@ void KCrash::initialize()
     if (s_launchDrKonqi == 0) { // disabled by the program itself
         return;
     }
+    if (shouldHandleCrash()) { // disabled by environment
+        return;
+    }
+
     const QStringList args = QCoreApplication::arguments();
-    if (shouldHandleCrash() && shouldUseDrKonqi()) {
+    if (shouldUseDrKonqi()) {
         KCrash::setDrKonqiEnabled(true);
     } else {
         // This loads qtlogging.ini very early which prevents unittests from doing QStandardPaths::setTestModeEnabled(true) in initTestCase()
